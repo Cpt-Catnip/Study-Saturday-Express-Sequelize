4baf6028fe82fa6498b51875e34ef646f9aa94c2
Mike Koshakow <mzkosh@gmail.com>
2020-06-27 13:52:40 -0400

    established routes

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,48 @@
 const router = require('express').Router();
+const Student = require('../db/models/student')
 
+router.get('/', async (req, res, next) => {
+  try{
+    const data = await Student.findAll();
+    res.send(data)
+  } catch(err) { next(err) }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const data = await Student.create(req.body);
+    res.status(201).json(data);
+  } catch(err) { next(err) }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const student = await Student.findById(id)
+    if(!student) {
+      res.sendStatus(404)
+    } else {
+      res.status(200).send(student);
+    }
+  } catch(err) { next(err) }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    Object.keys(req.body)
+      .forEach(field => student[field] = req.body[field]);
+    await student.save()
+    res.status(200).json(student)
+  } catch(err) { next(err) }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try{
+    let student = await Student.findById(req.params.id);
+    await student.destroy();
+    res.sendStatus(204)
+  } catch(err) { next(err) }
+})
 
 module.exports = router;
--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,37 @@
 const router = require('express').Router();
+const Test = require('../db/models/test');
+const Student = require('../db/models/student');
 
+router.get('/', async (req, res, next) => {
+  try{
+    const tests = await Test.findAll();
+    res.status(200).json(tests);
+  } catch(err) { next(err) }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const test = await Test.findById(req.params.id);
+    res.status(200).json(test);
+  } catch(err) { next(err) }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const test = await Test.findById(req.params.id);
+    if (!test) return res.sendStatus(404);
+    await test.destroy();
+    res.sendStatus(204)
+  } catch(err) { next(err) }
+})
+
+router.post('/student/:studentId', async (req, res, next) => {
+  try{
+    const student = await Student.findById(req.params.studentId);
+    if (!student) return res.sendStatus(404);
+    const test = await student.createTest(req.body);
+    res.status(201).json(test)
+  } catch(err) { next(err) }
+})
 
 module.exports = router;
